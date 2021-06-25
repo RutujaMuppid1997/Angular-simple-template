90105245b8ea7d8468f2cd1fabce755b635fd5c2
Rutuja Muppid <RutujaM@winjit.com>
2021-06-25 12:26:17 +0530

    minor changes

--- src/app/const/api.ts
+++ src/app/const/api.ts
@@ -1,6 +1,8 @@
 
 export var API ={
 
-nodeEndPoint:"http://etstaging.centralindia.cloudapp.azure.com:5524/",
-web3EndPoint :"http://etstaging.centralindia.cloudapp.azure.com:5522/api/v1.0/",
+// nodeEndPoint:"http://etstaging.centralindia.cloudapp.azure.com:5524/",
+// web3EndPoint :"http://etstaging.centralindia.cloudapp.azure.com:5522/api/v1.0/",
+nodeEndPoint:"http://localhost:5524/",
+web3EndPoint :"http://localhost:5522/api/v1.0/",
 } 
